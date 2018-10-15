3fb1e4511248b1a8cfe0a8d068c331f9bb620dad
junxian shen <sjx15@mails.tsinghua.edu.cn>
2018-10-15 21:34:45 +0800

    change sync.sh

--- sync.sh
+++ sync.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 #scp -r mtnf amax@202.112.237.33:/home/amax/hy/MultiTenantNF
-scp -r mtnf ubuntu@202.112.237.65:/home/ubuntu/mtnf
+scp -r mtnf ubuntu@202.112.237.65:/home/ubuntu/MTNF/mtnf
