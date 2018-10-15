acacb2154280925c6a072014c8ddf70a4eb3bf95
junxian shen <sjx15@mails.tsinghua.edu.cn>
2018-10-15 15:51:58 +0800

    add port bind script

---
+++ mtnf/framework/bind_port.sh
@@ -0,0 +1,4 @@
+sudo ~/dpdk-stable-18.02.2/usertools/dpdk-devbind.py --bind=igb_uio vf0_0
+sudo ~/dpdk-stable-18.02.2/usertools/dpdk-devbind.py --bind=igb_uio vf0_1
+sudo ~/dpdk-stable-18.02.2/usertools/dpdk-devbind.py --bind=igb_uio vf0_2
+sudo ~/dpdk-stable-18.02.2/usertools/dpdk-devbind.py --bind=igb_uio vf0_3
